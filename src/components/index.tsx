aab127d7b1c4d4fe929a2ebf6aa8a61c5c1ecadd
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-19 20:17:05 +0530

    add: Loader, FeaturedProduct

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,15 @@ import Footer from "./Footer/footer";
 import SelectWithSearch from "./SelectWithSearch/selectWithSearch";
 import BorderAndOr from "./BorderAndOr/borderAndOr";
 import OtpBox from "./OtpBox/otpBox";
+import FeaturedProduct from "./FeaturedProduct/featuredProduct";
+import Loader from "./Loader/loader";
 
-export { Header, Footer, SelectWithSearch, BorderAndOr, OtpBox };
+export {
+    Header,
+    Footer,
+    SelectWithSearch,
+    BorderAndOr,
+    OtpBox,
+    FeaturedProduct,
+    Loader,
+};
