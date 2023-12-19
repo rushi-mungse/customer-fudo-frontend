ce5a09f9a63c276c2da3e0b365351f2e2c920825
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-19 20:19:38 +0530

    add: routers

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,20 +1,51 @@
 import {
     createBrowserRouter,
     createRoutesFromElements,
+    Navigate,
+    Outlet,
     Route,
 } from "react-router-dom";
+import { useAppSelector } from "../hooks/reduxHooks";
 import Layout from "../Layout";
-import { HomePage, OtpVerification, SignUp } from "../pages";
+import {
+    ForgetPassword,
+    HomePage,
+    Login,
+    OtpVerification,
+    ResetPassword,
+    SignUp,
+    UserDashBoard,
+} from "../pages";
+import { RootState } from "../state/store";
+
+const GuestRoute = () => {
+    const isAuth = useAppSelector(
+        (state: RootState) => state.authReducer.isAuth
+    );
+    return isAuth ? <Navigate to="/" /> : <Outlet />;
+};
+
+const ProtectedRoute = () => {
+    const isAuth = useAppSelector(
+        (state: RootState) => state.authReducer.isAuth
+    );
+    return !isAuth ? <Navigate to="/" /> : <Outlet />;
+};
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Layout />}>
             <Route path="" element={<HomePage />} />
-            <Route path="/auth/signup" element={<SignUp />} />
-            <Route
-                path="/auth/otp-verification"
-                element={<OtpVerification />}
-            />
+            <Route path="auth" element={<GuestRoute />}>
+                <Route path="login" element={<Login />} />
+                <Route path="signup" element={<SignUp />} />
+                <Route path="otp-verification" element={<OtpVerification />} />
+                <Route path="forget-password" element={<ForgetPassword />} />
+                <Route path="set-password" element={<ResetPassword />} />
+            </Route>
+            <Route path="user" element={<ProtectedRoute />}>
+                <Route path="" element={<UserDashBoard />} />
+            </Route>
         </Route>
     )
 );
