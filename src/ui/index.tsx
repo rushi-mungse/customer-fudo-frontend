c1da6f5ef2606c9b56427bbd88fda280acf0524e
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-19 20:20:26 +0530

    add: product card

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -2,5 +2,6 @@ import Button from "./Button/button";
 import Logo from "./Logo/logo";
 import HeaderLinks from "./HeaderLinks/headerLinks";
 import InputWithSearch from "./InputWithSearch/inputWithSearch";
+import ProductCard from "./ProductCard/productCard";
 
-export { Button, Logo, HeaderLinks, InputWithSearch };
+export { Button, Logo, HeaderLinks, InputWithSearch, ProductCard };
