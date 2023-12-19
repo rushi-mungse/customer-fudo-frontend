203b601a54f959a1511f4e69a5088bdaaa68ee59
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-19 20:18:18 +0530

    add: userDashBoard, Forget Password pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
 import HomePage from "./Home/home";
 import SignUp from "./auth/SignUp/signUp";
 import OtpVerification from "./auth/OtpVerification/otpVerification";
+import Login from "./auth/Login/login";
+import UserDashBoard from "./UserDashBoard/userDashBoard";
+import ForgetPassword from "./auth/ForgetPassword/forgetPassword";
+import ResetPassword from "./auth/ResetPassword/resetPassword";
 
-export { HomePage, SignUp, OtpVerification };
+export {
+    HomePage,
+    SignUp,
+    OtpVerification,
+    Login,
+    UserDashBoard,
+    ForgetPassword,
+    ResetPassword,
+};
