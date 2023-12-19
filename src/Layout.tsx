4a516569fa90838f666362b09bcfe005677c4b1b
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-19 20:16:39 +0530

    add: refresh hook and loader

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,24 @@
 import { Outlet } from "react-router-dom";
 import { Header, Footer } from "./components";
+import useRefreshHook from "./hooks/useRefresh";
+import { Spin } from "antd";
 
 const Layout = () => {
+    const { loading } = useRefreshHook();
+
     return (
         <main className="bg-bgColor">
-            <Header />
-            <Outlet />
-            <Footer />
+            {loading ? (
+                <Spin tip="" size="large">
+                    <div className="content" />
+                </Spin>
+            ) : (
+                <>
+                    <Header />
+                    <Outlet />
+                    <Footer />
+                </>
+            )}
         </main>
     );
 };
