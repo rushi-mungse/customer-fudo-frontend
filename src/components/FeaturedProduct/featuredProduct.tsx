6049917ae9d7f1ef53c48c0dfecb5a93c65e2729
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-19 20:17:17 +0530

    add: Featured Product Section

---
+++ src/components/FeaturedProduct/featuredProduct.tsx
@@ -0,0 +1,30 @@
+import { ProductCard } from "../../ui";
+import { Link } from "react-router-dom";
+
+const FeaturedProduct = () => {
+    return (
+        <div className="min-h-screen min-w-screen">
+            <div className="container mx-auto">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-4xl tracking-wider leading-20 font-semibold text-dark/90">
+                        Our Featured Food
+                    </h1>
+                    <Link to="/menu">
+                        <span className="text-active hover:text-active/80 text-sm">
+                            See all Food ⭢
+                        </span>
+                    </Link>
+                </div>
+            </div>
+
+            <div className="grid grid-cols-2 gap-8 container mx-auto py-8">
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedProduct;
