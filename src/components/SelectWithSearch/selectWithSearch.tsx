5ee528ddf57cf36571586435a6737d4e3b0ed993
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-19 20:17:47 +0530

    add: Select Box With Search Button

--- src/components/SelectWithSearch/selectWithSearch.tsx
+++ src/components/SelectWithSearch/selectWithSearch.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Select } from "antd";
+import { useQuery } from "react-query";
+import { getTenants } from "../../services/api/api";
+import { Loader } from "../";
 
 const onChange = (value: string) => {
     console.log(`selected ${value}`);
@@ -15,23 +18,32 @@ const filterOption = (
     option?: { label: string; value: string }
 ) => (option?.label ?? "").toLowerCase().includes(input.toLowerCase());
 
-const SelectWithSerch: React.FC = () => (
-    <Select
-        showSearch
-        bordered={false}
-        placeholder="Select a Resto"
-        optionFilterProp="children"
-        style={{ width: "100%" }}
-        onChange={onChange}
-        onSearch={onSearch}
-        filterOption={filterOption}
-        options={[
-            {
-                value: "jack",
-                label: "ack and me",
-            },
-        ]}
-    />
-);
+const SelectWithSerch: React.FC = () => {
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["tenantData"],
+        queryFn: async () => await getTenants(),
+        staleTime: 10000,
+    });
+
+    if (isLoading) return <Loader tip="" size="small" className="text-sm" />;
+    return (
+        <Select
+            showSearch
+            bordered={false}
+            placeholder="Select a Resto"
+            optionFilterProp="children"
+            style={{ width: "100%" }}
+            onChange={onChange}
+            onSearch={onSearch}
+            filterOption={filterOption}
+            options={[
+                {
+                    value: "jack",
+                    label: "ack and me",
+                },
+            ]}
+        />
+    );
+};
 
 export default SelectWithSerch;
