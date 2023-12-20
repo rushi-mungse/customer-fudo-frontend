5619dbc1fcb6c165d1a28b2e4456dfc8cc788a42
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-20 22:34:25 +0530

    add: app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { RouterProvider } from "react-router-dom";
 import { ConfigProvider } from "antd";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { store } from "./state/store";
-import router from "./router";
+import App from "./App";
 import "./index.css";
 import "antd/dist/reset.css";
 
@@ -20,7 +19,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         >
             <Provider store={store}>
                 <QueryClientProvider client={new QueryClient()}>
-                    <RouterProvider router={router} />
+                    <App />
                 </QueryClientProvider>
             </Provider>
         </ConfigProvider>
