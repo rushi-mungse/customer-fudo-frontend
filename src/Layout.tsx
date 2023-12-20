ccd5f16aab73d602202b355d1f4c2e40c32d4f83
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-20 22:34:12 +0530

    remove: loader

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,24 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Header, Footer } from "./components";
-import useRefreshHook from "./hooks/useRefresh";
-import { Spin } from "antd";
 
 const Layout = () => {
-    const { loading } = useRefreshHook();
-
     return (
         <main className="bg-bgColor">
-            {loading ? (
-                <Spin tip="" size="large">
-                    <div className="content" />
-                </Spin>
-            ) : (
-                <>
-                    <Header />
-                    <Outlet />
-                    <Footer />
-                </>
-            )}
+            <Header />
+            <Outlet />
+            <Footer />
         </main>
     );
 };
