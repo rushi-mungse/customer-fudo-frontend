e91519d540dc2513f3661d2b5ca7780fd416f96a
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-28 19:00:22 +0530

    feat: email and phone number change ui

---
+++ src/components/ChangeEmailAndPhoneNumber/changeEmailAndPhoneNumber.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Tabs } from "antd";
+import type { TabsProps } from "antd";
+import ChangeEmail from "./ChangeEmail/changeEmail";
+import ChangePhoneNumber from "./ChangePhoneNumber/changePhoneNumber";
+
+const items: TabsProps["items"] = [
+    {
+        key: "1",
+        label: "Change Email",
+        children: <ChangeEmail />,
+    },
+    {
+        key: "2",
+        label: "Change Phone Number",
+        children: <ChangePhoneNumber />,
+    },
+];
+
+const ChangeEmailAndPhoneNumber = () => {
+    const [key, setKey] = useState("1");
+    const onChange = (key: string) => setKey(key);
+    return (
+        <div className="shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-md bg-pure p-6 h-full">
+            <Tabs defaultActiveKey={key} items={items} onChange={onChange} />
+        </div>
+    );
+};
+
+export default ChangeEmailAndPhoneNumber;
