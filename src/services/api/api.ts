ae8610fc7ad6093fcee2e4da1c6ab5be30951218
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-31 16:51:04 +0530

    add: api endpoint functions

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,5 +1,5 @@
 import {
-    IForgotPasswordData,
+    IEmailData,
     ILoginData,
     IResetPasswordData,
     ISendOtpData,
@@ -21,7 +21,7 @@ export const getTenants = () => api.get("/tenant");
 
 export const logout = () => api.get("/auth/logout");
 
-export const forgetPassword = (data: IForgotPasswordData) =>
+export const forgetPassword = (data: IEmailData) =>
     api.post("/auth/forget-password", data);
 
 export const setPassword = (data: IResetPasswordData) =>
@@ -32,5 +32,17 @@ export const updateFullName = (data: IUpdateFullNameData) =>
 
 export const changePassword = (data: IChangePasswordData) =>
     api.post("/user/change-password", data);
+
+export const sendVerificationCodeOldEmail = (data: IEmailData) =>
+    api.post("/user/send-otp-for-email-change", data);
+
+export const verifySentOtpOldEmail = (data: IVerifyOtpData) =>
+    api.post("/user/verify-otp-for-email-change", data);
+
+export const sendVerificationCodeNewEmail = (data: IEmailData) =>
+    api.post("/user/send-otp-to-new-email-for-email-change", data);
+
+export const verifySentOtpNewEmail = (data: IVerifyOtpData) =>
+    api.post("/user/verify-new-email-for-email-change", data);
 
 export const self = () => api.get("/auth/self");
