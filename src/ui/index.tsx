a01c91587d8a65e9ea2ec25dbcfff66acc6ff0c4
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-01 20:45:18 +0530

    add: Serve ui component

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -3,5 +3,6 @@ import Logo from "./Logo/logo";
 import HeaderLinks from "./HeaderLinks/headerLinks";
 import InputWithSearch from "./InputWithSearch/inputWithSearch";
 import ProductCard from "./ProductCard/productCard";
+import Serve from "./Serve/serve";
 
-export { Button, Logo, HeaderLinks, InputWithSearch, ProductCard };
+export { Button, Logo, HeaderLinks, InputWithSearch, ProductCard, Serve };
