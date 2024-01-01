43d758622abf1295c95491773120de5fbe4e9f1d
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-01 20:45:04 +0530

    add: what we serve section

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,44 +1,81 @@
 import { Link } from "react-router-dom";
-import { FeaturedProduct } from "../../components";
+import { FeaturedProduct, Review, WhatWeServe } from "../../components";
 import { Button } from "../../ui";
+import { Rate } from "antd";
 
 export default function HomePage() {
     return (
         <div className="min-h-screen pt-[48px]">
-            <div className="container mx-auto h-screen">
-                <div className="flex items-center justify-center h-full">
-                    <div className="w-1/2 flex justify-center flex-col">
-                        <div className="text-5xl font-bold tracking-wider text-dark">
-                            <h1>
-                                Your Favourite{" "}
-                                <span className="text-active italic">food</span>
-                            </h1>
-                            <h1 className="leading-relaxed">
-                                <span className="text-active italic">
-                                    delivery
-                                </span>{" "}
-                                Partner Fudo
-                            </h1>
+            <section className="min-h-screen">
+                <div className="container mx-auto">
+                    <div className="flex-center min-h-screen">
+                        <div className="w-1/2 flex justify-center flex-col">
+                            <div className="text-5xl font-bold tracking-wider text-dark">
+                                <h1>
+                                    Your Favourite{" "}
+                                    <span className="text-active italic">
+                                        food
+                                    </span>
+                                </h1>
+                                <h1 className="leading-relaxed">
+                                    <span className="text-active italic">
+                                        delivery
+                                    </span>{" "}
+                                    Partner Fudo
+                                </h1>
+                            </div>
+                            <p className="text-dark/80 w-[80%] mt-1">
+                                We are focused on being the best helping hand to
+                                local bussinesses
+                            </p>
+
+                            <Link to="/menu" className="mt-12">
+                                <Button
+                                    intent="tertiary"
+                                    size="lg"
+                                    rounded="small"
+                                >
+                                    Order Now
+                                </Button>
+                            </Link>
+
+                            <div className="mt-8 flex items-center">
+                                <Review />
+                                <div className="flex flex-col ml-4">
+                                    <span className="font-semibold tracking-wide">
+                                        Our Happy Customer
+                                    </span>
+                                    <span>
+                                        <Rate
+                                            disabled
+                                            count={1}
+                                            defaultValue={4}
+                                            className="text-sm"
+                                        />
+                                        <Link
+                                            to={"/reviews"}
+                                            className="text-sm text-active ml-2 hover:text-active-600"
+                                        >
+                                            (12.5k Reviews)
+                                        </Link>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
-                        <p className="text-dark/80 w-[80%] mt-1">
-                            We are focused on being the best helping hand to
-                            local bussinesses
-                        </p>
-
-                        <Link to="/menu" className="mt-12">
-                            <Button intent="tertiary" size="lg" rounded="small">
-                                Order Now
-                            </Button>
-                        </Link>
-
-                        <div className="mt-8 h-20">reviews</div>
-                    </div>
-                    <div className="w-1/2">
-                        <img src="/food.png" alt="food" />
+                        <div className="w-1/2">
+                            <img src="/food.png" alt="food" />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <FeaturedProduct />
+            </section>
+            <section className="bg-gradient-to-bl from-red-100 via-red-50 to-gray-50 rounded-b-xl min-h-screen">
+                <div className="container mx-auto py-16 ">
+                    <WhatWeServe />
+                </div>
+            </section>
+            <section className="mt-8">
+                <FeaturedProduct />
+            </section>
         </div>
     );
 }
