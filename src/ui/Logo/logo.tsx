5706fe581682558fc6571f69aba8e554cece091c
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-01 07:52:26 +0530

    add: modify logo component

--- src/ui/Logo/logo.tsx
+++ src/ui/Logo/logo.tsx
@@ -2,7 +2,10 @@ import { Link } from "react-router-dom";
 
 const Logo = () => {
     return (
-        <Link to={"/"}>
+        <Link to={"/"} className="flex items-center justify-center">
+            <span className="bg-active rounded-[50%] w-[16px] h-[16px] flex-center mr-2">
+                <span className="bg-pure rounded-[50%] w-[25%] h-[25%]"></span>
+            </span>
             <div className="text-xl font-semibold tracking-wide leading-3 text-dark/80">
                 <span>F</span>
                 <span className="text-active">UD</span>
