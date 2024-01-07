a2e0f42f4171839ed061279690d6d612b990dd81
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-07 11:13:42 +0530

    add: add user component

--- src/pages/user/AddUser.tsx
+++ src/pages/user/AddUser.tsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import { SendOtpStep, VerifyOtpStep } from "../../components";
+
 const AddUser = () => {
-    return <div>AddUser</div>;
+    const [disabled, setDisabled] = useState(true);
+    return (
+        <div className="w-full h-full flex-center gap-8">
+            <SendOtpStep disabled={disabled} setDisabled={setDisabled} />
+            <VerifyOtpStep disabled={disabled} setDisabled={setDisabled} />
+        </div>
+    );
 };
 
 export default AddUser;
