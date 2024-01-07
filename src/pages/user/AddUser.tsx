36b57e94b1aff46037de54012ba5b3c936e493ff
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-07 14:13:34 +0530

    add: user components

--- src/pages/user/AddUser.tsx
+++ src/pages/user/AddUser.tsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 import { SendOtpStep, VerifyOtpStep } from "../../components";
+import { Card } from "antd";
 
 const AddUser = () => {
     const [disabled, setDisabled] = useState(true);
     return (
-        <div className="w-full h-full flex-center gap-8">
-            <SendOtpStep disabled={disabled} setDisabled={setDisabled} />
-            <VerifyOtpStep disabled={disabled} setDisabled={setDisabled} />
-        </div>
+        <>
+            <Card className="mb-8">
+                <span className="text-active font-bold">Add User</span>
+            </Card>
+            <div className="flex-center gap-8">
+                <SendOtpStep disabled={disabled} setDisabled={setDisabled} />
+                <VerifyOtpStep disabled={disabled} setDisabled={setDisabled} />
+            </div>
+        </>
     );
 };
 
