88edf3c4c191c6084ae79e0492673450617243dd
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-09 08:43:48 +0530

    add: prduct axios requests

---
+++ src/services/api/product.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL_5002,
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+});
+
+export const addProduct = async (data: FormData) => {
+    return await axios({
+        method: "POST",
+        url: `${import.meta.env.VITE_API_BASE_URL_5002}/product/create`,
+        data,
+        withCredentials: true,
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
+};
+
+export const getProducts = async () => api.get("/product");
+export const deleteProduct = async (productId: number) =>
+    api.delete(`/product/${productId}`);
+export const getProduct = async (productId: number) =>
+    api.get(`/product/${productId}`);
